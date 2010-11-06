540dbb3e6d80a13b5f6085b0ed7b7d44fe50efd2
Jan Lindblom <lindblom.jan@gmail.com>
2010-11-06 14:52:42 +0100

    some more stuff

--- src/se/janlindblom/toy/arch/lib/ALU.java
+++ src/se/janlindblom/toy/arch/lib/ALU.java
@@ -3,14 +3,15 @@
  */
 package se.janlindblom.toy.arch.lib;
 
-import se.janlindblom.toy.arch.lib.type.OpCode;
+import se.janlindblom.toy.arch.lib.OpCodeBus;
+import se.janlindblom.toy.arch.lib.Wire;
 
 /**
  * @author Jan Lindblom (lindblom.jan@gmail.com)
  *
  */
 public class ALU {
-	private OpCode opcode;
+	private OpCodeBus opcode;
 	private Wire writeRAM;
 	private Wire incPC;
 	private Wire clockPC;
